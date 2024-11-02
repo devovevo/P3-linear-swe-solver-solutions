ce4cd56f95c39eb340aa9d2da455255a9b84cb7d
erv29 <evan.vera@hotmail.com>
2024-11-01 17:32:08 -0700

    Fixed problem with basic serial

--- gpu/gpu.cu
+++ gpu/gpu.cu
@@ -113,10 +113,90 @@ void swap_buffers()
     dv = tmp;
 }
 
+void __global__ combined_kernel(double *h, double *u, double *v, double *dh, double *du, double *dv, double *dh1, double *du1, double *dv1, double *dh2, double *du2, double *dv2, int nx, int ny, double dx, double dy, double g, double H, double dt, unsigned int nsteps)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if (i >= nx || j >= ny)
+        return;
+
+    for (unsigned int t = 0; t < nsteps; t++)
+    {
+        if (i == nx - 1)
+        {
+            h(nx, j) = h(0, j);
+        }
+
+        if (j == ny - 1)
+        {
+            h(i, ny) = h(i, 0);
+        }
+
+        dh(i, j) = -H * (du_dx(i, j) + dv_dy(i, j));
+        du(i, j) = -g * dh_dx(i, j);
+        dv(i, j) = -g * dh_dy(i, j);
+
+        __syncthreads();
+
+        double a1, a2, a3;
+
+        if (t == 0)
+        {
+            a1 = 1.0;
+        }
+        else if (t == 1)
+        {
+            a1 = 3.0 / 2.0;
+            a2 = -1.0 / 2.0;
+        }
+        else
+        {
+            a1 = 23.0 / 12.0;
+            a2 = -16.0 / 12.0;
+            a3 = 5.0 / 12.0;
+        }
+
+        h(i, j) += (a1 * dh(i, j) + a2 * dh1(i, j) + a3 * dh2(i, j)) * dt;
+        u(i + 1, j) += (a1 * du(i, j) + a2 * du1(i, j) + a3 * du2(i, j)) * dt;
+        v(i, j + 1) += (a1 * dv(i, j) + a2 * dv1(i, j) + a3 * dv2(i, j)) * dt;
+
+        if (i == nx - 1)
+        {
+            u(0, j) = u(nx, j);
+        }
+
+        if (j == ny - 1)
+        {
+            v(i, 0) = v(i, ny);
+        }
+
+        double *tmp;
+
+        tmp = dh2;
+        dh2 = dh1;
+        dh1 = dh;
+        dh = tmp;
+
+        tmp = du2;
+        du2 = du1;
+        du1 = du;
+        du = tmp;
+
+        tmp = dv2;
+        dv2 = dv1;
+        dv1 = dv;
+        dv = tmp;
+    }
+}
+
 int t = 0;
 
 void step()
 {
+    dim3 threadsPerBlock(16, 16);
+    dim3 numBlocks(nx / threadsPerBlock.x + 1, ny / threadsPerBlock.y + 1);
+
     double a1, a2, a3;
 
     if (t == 0)
@@ -135,11 +215,12 @@ void step()
         a3 = 5.0 / 12.0;
     }
 
-    dim3 threadsPerBlock(16, 16);
-    dim3 numBlocks(nx / threadsPerBlock.x + 1, ny / threadsPerBlock.y + 1);
-
     compute_derivs<<<numBlocks, threadsPerBlock>>>(h, u, v, du, dv, dh, nx, ny, dx, dy, g, H);
     update_fields<<<numBlocks, threadsPerBlock>>>(h, u, v, dh, du, dv, dh1, du1, dv1, dh2, du2, dv2, nx, ny, dx, dy, dt, a1, a2, a3);
+    // if (t % 20 == 0)
+    // {
+    //     combined_kernel<<<numBlocks, threadsPerBlock>>>(h, u, v, dh, du, dv, dh1, du1, dv1, dh2, du2, dv2, nx, ny, dx, dy, g, H, dt, 20);
+    // }
 
     swap_buffers();
 
