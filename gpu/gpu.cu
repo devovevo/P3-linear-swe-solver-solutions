50ff269e117b4fc8c194b3385e4555c5021495f4
Evan Vera <erv29@cornell.edu>
2025-03-15 20:47:26 +1100

    B

--- gpu/gpu.cu
+++ gpu/gpu.cu
@@ -212,23 +212,20 @@ __global__ void kernel(float *h, float *u, float *v, float *dh1, float *du1, flo
 
         const int local_idx = i / blockDim.x;
 
-        if (grid_x < 0 || grid_y < 0 || grid_x >= nx || grid_y >= ny)
-        {
-            continue;
-        }
-
-        const int grid_idx = grid_x * ny + grid_y;
-        const int thread_idx = thread_x * halo_block_dims[1] + thread_y;
+        // if (grid_x < 0 || grid_y < 0 || grid_x >= nx || grid_y >= ny)
+        // {
+        //     continue;
+        // }
 
         // printf("Thread %d of block (%d, %d) is loading in from block (%d, %d) and local idx %d and writing back into grid (%d, %d). The corresponding block h value is %f and the grid h value is %f.\n", threadIdx.x, blockIdx.x, blockIdx.y, thread_x, thread_y, local_idx, grid_x, grid_y, block_h(thread_x, thread_y), h(grid_x, grid_y));
 
-        h[grid_idx] = block_h[thread_idx];
-        u[grid_idx] = block_u[thread_idx];
-        v[grid_idx] = block_v[thread_idx];
+        h(grid_x, grid_y) = block_h(thread_x, thread_y);
+        u(grid_x, grid_y) = block_u(thread_x, thread_y);
+        // v(grid_x, grid_y) = block_v(thread_x, thread_y);
 
-        dh1[grid_idx] = thread_dh1[local_idx];
-        du1[grid_idx] = thread_du1[local_idx];
-        dv1[grid_idx] = thread_dv1[local_idx];
+        // dh1(grid_x, grid_y) = thread_dh1[local_idx];
+        // du1(grid_x, grid_y) = thread_du1[local_idx];
+        // dv1(grid_x, grid_y) = thread_dv1[local_idx];
     }
 }
 
