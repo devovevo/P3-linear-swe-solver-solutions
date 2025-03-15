16bbcbe69ef35359d1d5185a44c828f3fceb4051
Evan Vera <erv29@cornell.edu>
2025-03-15 19:58:35 +1100

    B

--- gpu/gpu.cu
+++ gpu/gpu.cu
@@ -153,10 +153,10 @@ __global__ void kernel(float *h, float *u, float *v, float *dh1, float *du1, flo
         int thread_x = i / halo_block_dims[0];
         int thread_y = i % halo_block_dims[0];
 
-        int grid_x = mod(block_x * block_dims[0] + thread_x - BLOCK_HALO_RAD, nx);
-        int grid_y = mod(block_y * block_dims[1] + thread_y - BLOCK_HALO_RAD, ny);
+        // int grid_x = mod(block_x * block_dims[0] + thread_x - BLOCK_HALO_RAD, nx);
+        // int grid_y = mod(block_y * block_dims[1] + thread_y - BLOCK_HALO_RAD, ny);
 
-        int local_idx = i / blockDim.x;
+        // int local_idx = i / blockDim.x;
 
         // block_h(thread_x, thread_y) = h(grid_x, grid_y);
         // block_u(thread_x, thread_y) = u(grid_x, grid_y);
@@ -182,9 +182,9 @@ __global__ void kernel(float *h, float *u, float *v, float *dh1, float *du1, flo
 
         // __syncthreads();
 
-        swap(thread_dh, thread_dh1, MAX_THREAD_DIM * MAX_THREAD_DIM);
-        swap(thread_du, thread_du1, MAX_THREAD_DIM * MAX_THREAD_DIM);
-        swap(thread_dv, thread_dv1, MAX_THREAD_DIM * MAX_THREAD_DIM);
+        // swap(thread_dh, thread_dh1, MAX_THREAD_DIM * MAX_THREAD_DIM);
+        // swap(thread_du, thread_du1, MAX_THREAD_DIM * MAX_THREAD_DIM);
+        // swap(thread_dv, thread_dv1, MAX_THREAD_DIM * MAX_THREAD_DIM);
 
         t++;
     }
@@ -195,15 +195,15 @@ __global__ void kernel(float *h, float *u, float *v, float *dh1, float *du1, flo
         int thread_x = i / halo_block_dims[0];
         int thread_y = i % halo_block_dims[0];
 
-        int grid_x = block_x * block_dims[0] + thread_x - BLOCK_HALO_RAD;
-        int grid_y = block_y * block_dims[1] + thread_y - BLOCK_HALO_RAD;
+        // int grid_x = block_x * block_dims[0] + thread_x - BLOCK_HALO_RAD;
+        // int grid_y = block_y * block_dims[1] + thread_y - BLOCK_HALO_RAD;
 
-        int local_idx = i / blockDim.x;
+        // int local_idx = i / blockDim.x;
 
-        if (grid_x < 0 || grid_y < 0 || grid_x >= nx || grid_y >= ny)
-        {
-            continue;
-        }
+        // if (grid_x < 0 || grid_y < 0 || grid_x >= nx || grid_y >= ny)
+        // {
+        //     continue;
+        // }
 
         // printf("Thread %d of block (%d, %d) is loading in from block (%d, %d) and local idx %d and writing back into grid (%d, %d). The corresponding block h value is %f and the grid h value is %f.\n", threadIdx.x, blockIdx.x, blockIdx.y, thread_x, thread_y, local_idx, grid_x, grid_y, block_h(thread_x, thread_y), h(grid_x, grid_y));
 
