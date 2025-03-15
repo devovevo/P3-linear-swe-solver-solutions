35799a5cb7e0dd5b0d6203aa20462a26bdb636f5
Evan Vera <erv29@cornell.edu>
2025-03-15 19:23:27 +1100

    B40

--- gpu/gpu.cu
+++ gpu/gpu.cu
@@ -154,9 +154,9 @@ __global__ void kernel(float *h, float *u, float *v, float *dh1, float *du1, flo
 
         int local_idx = i / blockDim.x;
 
-        // block_h(thread_x, thread_y) = h(grid_x, grid_y);
-        // block_u(thread_x, thread_y) = u(grid_x, grid_y);
-        // block_v(thread_x, thread_y) = v(grid_x, grid_y);
+        block_h(thread_x, thread_y) = h(grid_x, grid_y);
+        block_u(thread_x, thread_y) = u(grid_x, grid_y);
+        block_v(thread_x, thread_y) = v(grid_x, grid_y);
 
         // printf("Thread %d of block (%d, %d) is loading in from grid (%d, %d) into block (%d, %d) and local idx %d. The corresponding block h value is %f and the grid h value is %f.\n", threadIdx.x, blockIdx.x, blockIdx.y, grid_x, grid_y, thread_x, thread_y, local_idx, block_h(thread_x, thread_y), h(grid_x, grid_y));
 
