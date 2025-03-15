521ef89f603121d46f47dda5c23a36a5c1bad23e
Evan Vera <erv29@cornell.edu>
2025-03-15 21:46:33 +1100

    B

--- gpu/gpu.cu
+++ gpu/gpu.cu
@@ -154,9 +154,9 @@ __global__ void kernel(float *const h, float *const u, float *const v, float *co
 
             int local_idx = i / blockDim.x;
 
-            block_h(thread_x, thread_y) += (a1 * thread_dh[local_idx] + a2 * thread_dh1[local_idx]) * dt;
-            block_u(thread_x + 1, thread_y) += (a1 * thread_du[local_idx] + a2 * thread_du1[local_idx]) * dt;
-            block_v(thread_x, thread_y + 1) += (a1 * thread_dv[local_idx] + a2 * thread_dv1[local_idx]) * dt;
+            // block_h(thread_x, thread_y) += (a1 * thread_dh[local_idx] + a2 * thread_dh1[local_idx]) * dt;
+            // block_u(thread_x + 1, thread_y) += (a1 * thread_du[local_idx] + a2 * thread_du1[local_idx]) * dt;
+            // block_v(thread_x, thread_y + 1) += (a1 * thread_dv[local_idx] + a2 * thread_dv1[local_idx]) * dt;
         }
 
         __syncthreads();
