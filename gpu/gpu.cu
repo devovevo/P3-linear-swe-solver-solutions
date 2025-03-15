bf82bdf35af6dfbf6878bf55a33035fb3213ef6b
Evan Vera <erv29@cornell.edu>
2025-03-15 20:45:29 +1100

    B

--- gpu/gpu.cu
+++ gpu/gpu.cu
@@ -225,9 +225,9 @@ __global__ void kernel(float *h, float *u, float *v, float *dh1, float *du1, flo
         u[grid_idx] = block_u(thread_x, thread_y);
         v[grid_idx] = block_v(thread_x, thread_y);
 
-        // dh1(grid_x, grid_y) = thread_dh1[local_idx];
-        // du1(grid_x, grid_y) = thread_du1[local_idx];
-        // dv1(grid_x, grid_y) = thread_dv1[local_idx];
+        dh1[grid_idx] = thread_dh1[local_idx];
+        du1[grid_idx] = thread_du1[local_idx];
+        dv1[grid_idx] = thread_dv1[local_idx];
     }
 }
 
