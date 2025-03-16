710e67125c071277095805dda1011c4dc541229e
Evan Vera <erv29@cornell.edu>
2025-03-16 21:03:39 +1100

    B

--- gpu/gpu.cu
+++ gpu/gpu.cu
@@ -72,7 +72,10 @@ __global__ void kernel(float *const h, float *const u, float *const v, float *co
     // To find how many grid points this block is responsible for in each
     // direction, we divide total num of points by the number of blocks
     // in each direction
-    const unsigned int block_dims[2] = {min(nx / gridDim.x, nx - blockIdx.x * nx / gridDim.x), min(ny / gridDim.y, ny - blockIdx.y * ny / gridDim.y)};
+    const unsigned int block_dims[2] = {nx / gridDim.x, ny / gridDim.y};
+    block_dims[0] = blockIdx.x == gridDim.x - 1 ? nx - block_dims[0] * (gridDim.x - 1) : block_dims[0];
+    block_dims[1] = blockIdx.y == gridDim.y - 1 ? ny - block_dims[1] * (gridDim.y - 1) : block_dims[1];
+
     const unsigned int halo_block_dims[2] = {block_dims[0] + 2 * BLOCK_HALO_RAD, block_dims[1] + 2 * BLOCK_HALO_RAD};
 
     // Here, we set up our local blocks fields using the maximum amount of memory
@@ -95,7 +98,7 @@ __global__ void kernel(float *const h, float *const u, float *const v, float *co
     float thread_du1[MAX_THREAD_DIM * MAX_THREAD_DIM];
     float thread_dv1[MAX_THREAD_DIM * MAX_THREAD_DIM];
 
-    // printf("Thread %d of block (%d, %d) reporting for duty! The block dims are (%d, %d).\n", threadIdx.x, blockIdx.x, blockIdx.y, block_dims[0], block_dims[1]);
+    printf("Thread %d of block (%d, %d) reporting for duty! The block dims are (%d, %d).\n", threadIdx.x, blockIdx.x, blockIdx.y, block_dims[0], block_dims[1]);
 
     // We initialize our local block fields here by reading in from the
     // corresponding grid fields
