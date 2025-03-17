6948fb1e54da944382173274c357d58ff0973c8b
Evan Vera <erv29@cornell.edu>
2025-03-17 18:42:05 +1100

    B

--- gpu/gpu.cu
+++ gpu/gpu.cu
@@ -189,39 +189,44 @@ __global__ void kernel(float *const h, float *const u, float *const v, float *co
     }
 }
 
-int t = 0;
-
-const int block_dims[2] = {60, 60};
+const int block_dims[2] = {32, 32};
 const int num_threads = 32 * 32;
 const int halo_rad = 10;
+
+void call_kernel()
+{
+    if (block_dims[0] <= halo_rad || block_dims[1] <= halo_rad)
+    {
+        printf("The provided halo radius is at least as big as one of the dimensions, meaning it's all halo, which doesn't make sense.\n");
+        return;
+    }
+
+    if (block_dims[0] * block_dims[1] > 64 * 64)
+    {
+        printf("The desired block size would require too much shared memory. Maximum thread block memory size can be at most that of (64, 64).\n");
+        return;
+    }
+
+    if (block_dims[0] * block_dims[1] / num_threads > MAX_THREAD_DIM * MAX_THREAD_DIM)
+    {
+        printf("The desired block size would require each thread to need to store too many local derivative calculations. The max result per thread is %d.\n", MAX_THREAD_DIM * MAX_THREAD_DIM);
+        return;
+    }
+
+    dim3 grid_dims(CEIL_DIV(nx, (block_dims[0] - 2 * halo_rad)), CEIL_DIV(ny, (block_dims[1] - 2 * halo_rad)));
+    dim3 thread_dims(num_threads);
+
+    kernel<halo_rad><<<grid_dims, thread_dims, 3 * block_dims[0] * block_dims[1] * sizeof(float)>>>(h, u, v, dh1, du1, dv1, nx, ny, t, dx, dy, dt, g, H);
+}
+
+int t = 0;
 
 void step()
 {
 
     if (t % (2 * halo_rad) == 0)
     {
-        if (block_dims[0] <= halo_rad || block_dims[1] <= halo_rad)
-        {
-            printf("The provided halo radius is at least as big as one of the dimensions, meaning it's all halo, which doesn't make sense.\n");
-            return;
-        }
-
-        if (block_dims[0] * block_dims[1] > 64 * 64)
-        {
-            printf("The desired block size would require too much shared memory. Maximum thread block memory size can be at most that of (64, 64).\n");
-            return;
-        }
-
-        if (block_dims[0] * block_dims[1] / num_threads > MAX_THREAD_DIM * MAX_THREAD_DIM)
-        {
-            printf("The desired block size would require each thread to need to store too many local derivative calculations. The max result per thread is %d.\n", MAX_THREAD_DIM * MAX_THREAD_DIM);
-            return;
-        }
-
-        dim3 grid_dims(CEIL_DIV(nx, (block_dims[0] - 2 * halo_rad)), CEIL_DIV(ny, (block_dims[1] - 2 * halo_rad)));
-        dim3 thread_dims(num_threads);
-
-        kernel<halo_rad><<<grid_dims, thread_dims, 3 * block_dims[0] * block_dims[1] * sizeof(float)>>>(h, u, v, dh1, du1, dv1, nx, ny, t, dx, dy, dt, g, H);
+        call_kernel();
     }
 
     t++;
